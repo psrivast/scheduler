3503bba4d7d310a03ed5f60f7bc77f1911d70411
Parag Srivastava <psrivast@andrew.cmu.edu>
2013-12-22 09:43:41 -0500

    Initial Commit

---
+++ Scheduler/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Scheduler
+//
+//  Created by Parag Srivastava on 12/22/13.
+//  Copyright (c) 2013 Parag Srivastava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
